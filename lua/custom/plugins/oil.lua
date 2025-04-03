82a9a09023f0dd99acb666371cecde9adc399065
Tony Dela Cruz <tony.delacruz@ballys.com>
2025-04-04 07:37:37 +0800

    updated oil config

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,9 +1,35 @@
 return {
   'stevearc/oil.nvim',
-  --@module 'oil'
-  --@type oil.SetupOpts
   config = function()
-    require('oil').setup()
+    local allowed_dotfiles = {
+      '.gitignore',
+      '.env',
+      '.env.local',
+      '.env.production',
+      '.eslintrc',
+      '.eslintrc.js',
+      '.eslintrc.json',
+      '.prettierrc',
+      '.prettierrc.js',
+      '.prettierrc.json',
+    }
+
+    ---@type oil.SetupOpts
+    require('oil').setup {
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          -- Check if it's a dotfile
+          if name:sub(1, 1) == '.' then
+            -- Hide it if it's NOT in the allowed list
+            return not vim.tbl_contains(allowed_dotfiles, name)
+          end
+
+          -- Show all non-dotfiles
+          return false
+        end,
+      },
+    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
   -- Optional dependencies
